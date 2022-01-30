192ba172e8962f52442fb1cfff7cc5a4913f841b
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-01-30 15:27:18 -0300

    Instanciando Tabs componentizadas  no home ✅

--- lib/src/screens/home_screen/home_screen.dart
+++ lib/src/screens/home_screen/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:icabannas/src/core/widgets/dish_button_widget.dart';
+import 'package:icabannas/src/screens/home_screen/tabs/favorite_tab_widget.dart';
+import 'package:icabannas/src/screens/home_screen/tabs/home_tab_widget.dart';
+import 'package:icabannas/src/screens/home_screen/tabs/last_orders_tab_widget.dart';
+import 'package:icabannas/src/screens/home_screen/tabs/profile_tab_widget.dart';
 import 'package:icabannas/src/screens/home_screen/widgets/app_bar_widget.dart';
 
 
@@ -21,98 +25,45 @@ class HomeScreen extends StatelessWidget {
           onMenuPressed: () => debugPrint('Drawer'),
           onCartPressed: () => debugPrint('Cart'),
         ),
-         body: TabBarView(
-          children: [
-            ConstrainedBox(
-              constraints: const BoxConstraints(maxHeight: 250),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                clipBehavior: Clip.none,
-                children: [
-                  DishButtonWidget(
-                    dishName: "Veggie tomato mix",
-                    dishPrice: "N1,900",
-                    dishImage: "veggie_tomato_mix",
-                    onPressed: () => print("Veggie tomato mix"),
-                  ),
-                  DishButtonWidget(
-                    dishName: "Egg and cucumber",
-                    dishPrice: "N1,500",
-                    dishImage: "egg_cucmber",
-                    onPressed: () => print("Egg and cucumber"),
-                  ),
-                  DishButtonWidget(
-                    dishName: "Fried Chicken m.",
-                    dishPrice: "N2,100",
-                    dishImage: "fried_chicken",
-                    onPressed: () => print("Fried Chicken m."),
-                  ),
-                  DishButtonWidget(
-                    dishName: "Moi-moi and ekpa.",
-                    dishPrice: "N2,30",
-                    dishImage: "moimoi",
-                    onPressed: () => print("Moi-moi and ekpa"),
-                  ),
-                ],
-              ),
-            ),                                  
-            Column(
-              children: const [
-                Icon(Icons.favorite),
-                Text("No Favorites yet",) 
-              ]
-            ),
-            Column(
-              children: const [
-                Icon(Icons.person),
-                Text("Build a screen to the profile here!",) 
-              ]
-            ),
-            Column(
-              children: const [
-                Icon(Icons.calendar_today),
-                Text("No history yet",) 
-              ]
-            ),
+         body: const TabBarView(
+          children: [                                                               
+            HomeTabWidget(),
+            FavoriteTabWidget(),
+            ProfileTabWidget(),
+            LastOrdersTabWidget(),
           ],
         ),
-        bottomNavigationBar: PreferredSize(
-          preferredSize: Size(
-            MediaQuery.of(context).size.width * 0.6,
-            MediaQuery.of(context).size.width * 0.2,
-          ), 
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.6,
-            height: MediaQuery.of(context).size.width * 0.2,
-            child: Theme(
-              data: ThemeData(
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-              ),
-              child: const TabBar(
-                indicator: BoxDecoration(
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0x33FA4A0C),
-                      blurRadius: 10.0,
-                      spreadRadius: 0.1,
-                    ),
-                  ],
-                ),
-                indicatorSize: TabBarIndicatorSize.label,
-                tabs: [
-                  Tab(icon: Icon(Icons.home, size: 35)),
-                  Tab(icon: Icon(Icons.favorite_border, size: 35)),
-                  Tab(icon: Icon(Icons.person_outline, size: 35)),
-                  Tab(icon: Icon(Icons.restore, size: 35)),
+        bottomNavigationBar: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.6,
+          height: MediaQuery.of(context).size.width * 0.2,
+          child: Theme(
+            data: ThemeData(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            ),
+            child: const TabBar(
+              indicator: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x33FA4A0C),
+                    blurRadius: 10.0,
+                    spreadRadius: 0.1,
+                  ),
                 ],
-                labelColor: Color(0xFFFA4A0C),
-                unselectedLabelColor: Color(0xFFADADAF),
               ),
+              indicatorSize: TabBarIndicatorSize.label,
+              tabs: [
+                Tab(icon: Icon(Icons.home, size: 35)),
+                Tab(icon: Icon(Icons.favorite_border, size: 35)),
+                Tab(icon: Icon(Icons.person_outline, size: 35)),
+                Tab(icon: Icon(Icons.restore, size: 35)),
+              ],
+              labelColor: Color(0xFFFA4A0C),
+              unselectedLabelColor: Color(0xFFADADAF),
             ),
           ),
-      ),
+        ),
       ),
     );
   }
